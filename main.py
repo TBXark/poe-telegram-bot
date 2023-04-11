31eaf2e92bfe60f48fc23f86e894a1b25202910a
TBXark <tbxark@outlook.com>
2023-04-11 10:17:28 +0800

    pref: 封装bot，添加markdown支持

--- main.py
+++ main.py
@@ -13,64 +13,80 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
-poe_configs = {}
+chat_bots = {}
 
 
-class PoeConfig:
-    def __init__(self, client, bot):
-        self.client = client
-        self.bot = bot
+class PoeChat:
+    def __init__(self, client_token, chat_model):
+        self.client = poe.Client(client_token)
+        self.model = chat_model
+
+    def chat(self, message):
+        return self.client.send_message(self.model, message)
+
+    def all_models(self):
+        return self.client.bot_names
+
+    def update_token(self, client_token):
+        self.client = poe.Client(client_token)
+
+    def update_model(self, chat_model):
+        self.model = chat_model
+
+    def new_conversation(self):
+        self.client.send_chat_break(self.model)
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    poe_cfg = poe_configs[str(update.message.from_user.id)]
-    if poe_cfg is None:
+    chat_bot = chat_bots[update.message.from_user.id]
+    if chat_bot is None:
         return
-    poe_cfg.client.send_chat_break(poe_cfg.bot)
+    chat_bot.new_conversation()
     logger.info("Started conversation with {}".format(update.message.from_user.id))
     await update.message.reply_text("New conversation started")
 
 
-async def roles(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    poe_cfg = poe_configs[str(update.message.from_user.id)]
-    if poe_cfg is None:
+async def models(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    chat_bot = chat_bots[update.message.from_user.id]
+    if chat_bot is None:
         return
-    names = poe_cfg.client.bot_names
+    names = chat_bot.all_models()
     text = ""
     for name in names:
         text += f"{name}: {names[name]}\n"
     await update.message.reply_text(text)
 
 
-async def role(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    poe_cfg = poe_configs[str(update.message.from_user.id)]
-    if poe_cfg is None:
+async def model(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    chat_bot = chat_bots[update.message.from_user.id]
+    if chat_bot is None:
         return
-    poe_cfg.bot = context.args[0]
-    await update.message.reply_text(f"Bot set to {poe_cfg.bot}")
+    new_model = context.args[0]
+    chat_bot.update_model(new_model)
+    await update.message.reply_text("Bot set to {}".format(new_model))
 
 
 async def token(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    poe_cfg = poe_configs[str(update.message.from_user.id)]
-    if poe_cfg is None:
+    chat_bot = chat_bots[update.message.from_user.id]
+    if chat_bot is None:
         return
-    poe_cfg.client = poe.Client(context.args[0])
+    chat_bot.update_token(context.args[0])
     await update.message.reply_text(f"Updated token")
 
 
 async def chat(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    poe_cfg = poe_configs.get(str(update.message.from_user.id))
-    if poe_cfg is None:
+    chat_bot = chat_bots[update.message.from_user.id]
+    if chat_bot is None:
         return
     msg = await update.message.reply_text("Thinking...")
     logger.info("Received message from {}: {}".format(update.message.from_user.id, update.message.text))
     full_text = ""
     edit_count = 0
     index = 0
-    for chunk in poe_cfg.client.send_message(poe_cfg.bot, update.message.text):
+    for chunk in chat_bot.chat(update.message.text):
         full_text = chunk["text"]
         index += 1
-        if index % 3 != 0:
+        if index % 4 != 0:
             continue
         if edit_count > 90:
             continue
@@ -79,15 +95,15 @@ async def chat(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
             edit_count += 1
         except Exception as e:
             logger.info("Failed to edit message: {}".format(str(e)))
-    await msg.edit_text(full_text)
+    await msg.edit_text(full_text, parse_mode="MarkdownV2")
 
 
-def set_my_commands(bot):
+async def set_my_commands(bot):
     try:
-        bot.set_my_commands([
+        await bot.set_my_commands([
             ("start", "Start a new conversation"),
-            ("roles", "List all roles"),
-            ("role", "Set the role"),
+            ("models", "List all models"),
+            ("model", "Set the model"),
             ("token", "Set the token"),
         ])
     except Exception as e:
@@ -95,7 +111,9 @@ def set_my_commands(bot):
 
 
 def main() -> None:
-    config_path = sys.argv[sys.argv.index("-c") + 1]
+    config_path = "config.json"
+    if len(sys.argv) > 1 and sys.argv.index("-c") < len(sys.argv) - 1:
+        config_path = sys.argv[sys.argv.index("-c") + 1]
     if config_path is None:
         logger.error("No config file specified")
         exit(1)
@@ -103,20 +121,18 @@ def main() -> None:
         config = json.load(f)
         telegram_token = config["token"]
         for user_id, user_config in config["chatbot"].items():
-            poe_configs[user_id] = PoeConfig(
-                poe.Client(user_config["token"]),
-                user_config["bot"]
-            )
+            chat_bots[int(user_id)] = PoeChat(user_config["token"], user_config["bot"])
 
     application = Application.builder().token(telegram_token).build()
     application.add_handler(CommandHandler("start", start))
-    application.add_handler(CommandHandler("roles", roles))
-    application.add_handler(CommandHandler("role", role))
+    application.add_handler(CommandHandler("models", models))
+    application.add_handler(CommandHandler("model", model))
     application.add_handler(CommandHandler("token", token))
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, chat))
 
     set_my_commands(application.bot)
 
+    logger.info("Starting application")
     application.run_polling()
 
 
